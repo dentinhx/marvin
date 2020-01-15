14d99d580863581441cd6752076249f628a43cec
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-15 19:32:28 -0300

    função mostranome e função nomedevolta

--- fase01/ex01/nomeDeVolta.js
+++ fase01/ex01/nomeDeVolta.js
@@ -1,13 +1,14 @@
+
 function mostraNome () {
    
    console.log("Marvin");
 }
 
-mostrarNome ()
+
 
 function nomeDeVolta () {
    
    console.log ("o andróide paranóide");
 }
 
-nomeDeVolta ()
+
