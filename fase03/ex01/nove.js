28f06ef0350ea1a90777b4ebeafae1a15061d7c0
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-24 19:54:35 -0300

    9?

---
+++ fase03/ex01/nove.js
@@ -0,0 +1,16 @@
+function nove(n){
+    var n1 = n.toString()
+    var n2 = n1.substring(n1.length-1,n1.length)
+    if (n2 === "9")
+    return "true"
+    else return "false"
+  
+  
+}
+
+
+
+
+
+
+
