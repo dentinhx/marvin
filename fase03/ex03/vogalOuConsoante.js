34971a5235ebc2ef6f286dbc577ee85f2f9151db
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-24 21:09:36 -0300

    vogal e consoante

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,25 @@
+function vogalOuConsoante(letra){
+if(letra=="A")
+return "Vogal"
+else if (letra =="E")
+return "Volgal"
+else if (letra =="I")
+return "Volgal"
+else if (letra =="O")
+return "Volgal"
+else if (letra =="U")
+return "Volgal"
+else if (letra =="a")
+return "Volgal"
+else if (letra =="e")
+return "Volgal"
+else if (letra =="i")
+return "Volgal"
+else if (letra =="o")
+return "Volgal"
+else if (letra =="u")
+return "Volgal"
+else 
+return "Consoante"
+}
+
