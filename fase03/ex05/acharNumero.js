d9e97b47a9e4216fca3ee6413a5e647120e8c355
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-27 18:50:28 -0300

    achando numero

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,12 @@
+function acharNumero(n1)   {
+    if (n1 < 5)
+    return "mercurio"
+    else if (n1 <=10 && n1 >=5 )
+    return "venus"
+    else if (n1 <=100 && n1 >=10)
+    return "terra"
+    else (n1 > 100)
+    return "marte"
+}
+
+
