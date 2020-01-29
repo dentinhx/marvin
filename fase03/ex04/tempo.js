af74712b0ba9a39eb4ec67c07e3bb028dd9c5e2a
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-29 18:20:55 -0300

    tempo

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -1,13 +1,20 @@
 function tempo(data1, data2, unidade){
-    var d = new Date(data1);
+    var d = new Date(data1)
     var d2 = new Date(data2)
     var diff = Math.abs(d2.getTime() - d.getTime())
-    var dias = Math.ceil(diff / (1000 * 60 * 60 * 24))
-    var mes = Math.ceil(diff / (1000 * 60 * 60 * 24 * 30))
-    var anos = Math.ceil(diff / (1000 * 60 *  ))
+    var dias = Math.round(diff / (1000 * 60 * 60 * 24))
+    var mes = Math.round(diff / (1000 * 60 * 60 * 24 * 30))
+    var anos = Math.round(diff /(1000 * 60 * 60 * 24 * 30 * 12))
+
+    if (unidade == "dia")
+    return dias
+    else if (unidade == "mês")
+    return mes
+    else
     return anos
-  
     
 }
+
+
 
 
