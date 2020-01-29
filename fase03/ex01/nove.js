faf425e3a0a54a0b8d7d3be69a25dacb6b9b2278
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-29 18:59:38 -0300

    nove

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -2,8 +2,8 @@ function nove(n){
     var n1 = n.toString()
     var n2 = n1.substring(n1.length-1,n1.length)
     if (n2 === "9")
-    return "true"
-    else return "false"
+    return true
+    else return false
   
   
 }
