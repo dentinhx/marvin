b08e748c1deb9f17e42984e154248cf78ba574bc
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-01-29 22:03:02 -0300

    ate zerinho

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,17 @@
+function ateZero(n1){
+    var z = 0
+    if (n1 >= z ){
+        while(n1>=z){
+                console.log(z)
+                z++
+        }
+        
+}else{
+     while(n1 <= z ){
+            console.log(n1)
+            n1++
+        }
+
+    }
+
+}
