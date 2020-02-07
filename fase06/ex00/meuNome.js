ec9cf7c75840eafd076bd3bd863889a59148b7b9
dentinhx <59850604+dentinhx@users.noreply.github.com>
2020-02-07 20:50:16 -0300

    MEU NOME

--- fase06/ex00/meuNome.js
+++ fase06/ex00/meuNome.js
@@ -1,9 +1,12 @@
 function meuNome(obj){
     var inicio = obj.nome.substring(0,1).toUpperCase();
     var fim = obj.nome.substring(1);
-    return inicio + fim;
+    var p = obj.nome.substring(1,obj.length)
+    var o = p.toLowerCase()
+    return inicio + o;
     
 }
+
 
 
 
